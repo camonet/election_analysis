5c84a67030de554fc9472b4be508356954fa8dc8
Cameron Monet Chapa <cameronmonet418@gmail.com>
2022-09-27 18:03:20 -0500

    -m "Practice"

---
+++ Python_practice.py
@@ -0,0 +1,13 @@
+print("help me")
+print("hi")
+type("hi")
+type("hello world")
+type(3.78899999999)
+type(-5)
+type(pi)
+ballots = (4, 18)
+type(ballots)
+help("keywords")
+listt = [1, 2, 'three']
+my_list = list()
+counties = ["Arapahoe", "Denver", 'Jefferson']
