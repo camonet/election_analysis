c2ced4a746fb04065cc12d4432f05acb7bcc0ad6
Cameron Monet Chapa <cameronmonet418@gmail.com>
2022-09-28 15:24:25 -0500

    -m "updated practice"

--- Python_practice.py
+++ Python_practice.py
@@ -11,3 +11,24 @@ help("keywords")
 listt = [1, 2, 'three']
 my_list = list()
 counties = ["Arapahoe", "Denver", 'Jefferson']
+counties
+counties[0]
+counties[-2]
+len(counties)
+counties.append('El Paso')
+counties
+counties.insert(2,'El Paso')
+counties.remove('El Paso')
+counties.pop(3)
+counties[2] = "El Paso"
+counties.append('El Paso')
+counties.remove('Arapahoe')
+counties[2] = 'Denver'
+counties[1] = 'Jefferson'
+counties.append('Arapahoe')
+counties.remove('Denver')
+counties[1] = 'El Paso'
+counties
+my_tuple = tuple()
+counties_tuple = ('Arapahoe', 'Denver', 'Jefferson')
+counties_tuple[:-1]
